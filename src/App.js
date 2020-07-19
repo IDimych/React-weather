e4023e6f1378eb8a7f403c78cbadc3fa59917d4d
IDimych <help-document@yandex.ru>
2020-07-19 12:59:51 +0300

    add props to WeatherDisplay

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ class App extends React.Component {
   render() {
     return (
         <div className="App">
-          <WeaterDisplay zip={"12345"} />
+          <WeatherDisplay zip={"12345"} />
         </div>
     )
   }
 }
 
-class WeaterDisplay extends React.Component {
+class WeatherDisplay extends React.Component {
   render() {
     return (
-        <h1>Displaying some Weather!</h1>
+        <h1>Displaying weather for city {this.props.zip}</h1>
     )
   }
 }
