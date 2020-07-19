7688dec35dc88aeb884fca69016af19dc861d849
IDimych <help-document@yandex.ru>
2020-07-19 12:56:53 +0300

    add class WeatherDisplay

--- src/App.js
+++ src/App.js
@@ -6,46 +6,20 @@ class App extends React.Component {
   render() {
     return (
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-             <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-              >
-              Learn React
-              </a>
-          </div>
+          <WeaterDisplay zip={"12345"} />
         </div>
+    )
+  }
+}
 
+class WeaterDisplay extends React.Component {
+  render() {
+    return (
+        <h1>Displaying some Weather!</h1>
     )
   }
 }
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
